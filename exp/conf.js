ebf4c8eab9cf433c194167740f19265b58442e93
joshkenney <joshuagkenney@gmail.com>
2022-10-07 10:49:56 -0400

    changed workerId assignment to random for testing

--- exp/conf.js
+++ exp/conf.js
@@ -1,5 +1,10 @@
-  //******************************************/
- //   Motion Change Determination            /
+//******************************************/
+//   Motion Change Determination            /
 //******************************************/
 
-let workerId = getParamFromURL( 'workerId' );
+// let workerId = getParamFromURL( 'workerId' );
+
+const workerId = Math.round(Math.random() * 10000, 2);
+
+const dark = "#000000";
+const light = "#ffffff";
