42e6ad54e58544a72074d56111bdb40314f1ad15
joshkenney <joshuagkenney@gmail.com>
2022-12-15 12:14:18 -0500

    added workerId assignment in validate.js

--- exp/conf.js
+++ exp/conf.js
@@ -2,6 +2,7 @@
 //   Motion Change Determination            /
 //******************************************/
 
+let workerId;
 // let workerId = getParamFromURL( 'workerId' );
 // Pilot experiment only
 // const workerId = Math.round(Math.random() * 10000, 2);
